f329b8f3b32ac447cc902594541b6da3b40251ad
ZhdanovDV <zhdanov.dv@students.dvfu.ru>
2022-01-11 17:02:58 +1000

    homework 2

---
+++ app/src/main/java/ru/fefu/activitytracker/fragments/MyActivityFragment.kt
@@ -0,0 +1,24 @@
+package ru.fefu.activitytracker.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ru.fefu.activitytracker.R
+
+class MyActivityFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_my_activity, container, false)
+    }
+
+}
